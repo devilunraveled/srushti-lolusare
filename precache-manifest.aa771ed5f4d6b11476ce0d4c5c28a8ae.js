9b65ccf68275a76971df6ba9bafee53e95ed0e42
Hardik Sharma <hardiksharmah73@gmail.com>
2024-12-31 22:54:05 +0530

    Updates

---
+++ precache-manifest.aa771ed5f4d6b11476ce0d4c5c28a8ae.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a132458b16f9bb4e8dec16d625087e37",
+    "url": "/srushti-lolusare/index.html"
+  },
+  {
+    "revision": "3b398b28dcb0f0491fb0",
+    "url": "/srushti-lolusare/static/css/2.cc663730.chunk.css"
+  },
+  {
+    "revision": "879fbb7eb572a279ee23",
+    "url": "/srushti-lolusare/static/css/main.759f2be0.chunk.css"
+  },
+  {
+    "revision": "3b398b28dcb0f0491fb0",
+    "url": "/srushti-lolusare/static/js/2.c4ca45e4.chunk.js"
+  },
+  {
+    "revision": "268cbd87c1870a295ec8",
+    "url": "/srushti-lolusare/static/js/3.b8a84e90.chunk.js"
+  },
+  {
+    "revision": "879fbb7eb572a279ee23",
+    "url": "/srushti-lolusare/static/js/main.302c6875.chunk.js"
+  },
+  {
+    "revision": "78112df29960f46fa806",
+    "url": "/srushti-lolusare/static/js/runtime~main.6b65caeb.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/srushti-lolusare/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/srushti-lolusare/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/srushti-lolusare/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/srushti-lolusare/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/srushti-lolusare/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/srushti-lolusare/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/srushti-lolusare/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/srushti-lolusare/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/srushti-lolusare/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/srushti-lolusare/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/srushti-lolusare/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "0971379bd37188b61b7ae93186ad87a4",
+    "url": "/srushti-lolusare/static/media/mainPhotoborder.0971379b.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/srushti-lolusare/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/srushti-lolusare/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/srushti-lolusare/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/srushti-lolusare/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/srushti-lolusare/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/srushti-lolusare/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/srushti-lolusare/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/srushti-lolusare/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/srushti-lolusare/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/srushti-lolusare/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/srushti-lolusare/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/srushti-lolusare/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/srushti-lolusare/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/srushti-lolusare/precache-manifest.aa771ed5f4d6b11476ce0d4c5c28a8ae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/srushti-lolusare/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
